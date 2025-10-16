93ba6f0d190958bdb640e23cc6eb2cc950c9862f
unknown <2300030270@kluniversity.in>
2025-10-16 07:58:35 +0530

    updated app properties

--- OVENAPI-SPRINGBOOT/src/main/java/com/klef/devops/OvenapiSpringbootApplication.java
+++ OVENAPI-SPRINGBOOT/src/main/java/com/klef/devops/OvenapiSpringbootApplication.java
@@ -2,10 +2,9 @@ package com.klef.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OvenapiSpringbootApplication extends SpringBootServletInitializer {
+public class OvenapiSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OvenapiSpringbootApplication.class, args);
